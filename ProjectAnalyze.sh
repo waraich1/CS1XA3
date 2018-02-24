79b1d510960ddbb57585352516d6642b025cf2de
waraich1 <35269086+waraich1@users.noreply.github.com>
2018-02-23 23:56:21 -0500

    Update ProjectAnalyze.sh

--- ProjectAnalyze.sh
+++ ProjectAnalyze.sh
@@ -1,4 +1,3 @@
-
 check_repositories()
 {
 git status -uall
@@ -7,11 +6,29 @@ check_uncommited(){
 git diff $1 >> changes.log
 }
 create_todo(){
-grep "#TODO" >> todo.log 
+(grep -r -h "#TODO" --exclude="ProjectAnanalyze.sh"--exclude="changes.log" --exclude="todo.log" .)  >> todo.log
 }
 
 error_hs(){
 find -name "*.hs" -exec ghc -fno-code {} \;&>error.log
 }
+check_flies(){
+if [ -f ~/harry.hs ]
+then
+         echo "The file exists"
+else
+         echo "the f does not exist"
+
+fi
+}
+More_coding(){
+a = $(find . -name '*.hs' | xargs wc -l)
+b = $(find . -name '*.py' | xargs wc -l)
+
+if [ "$a" -gt "$b"]; then
+    echo "There are more haskell code than python"
+elif ["$b" -gt "$a"]; then
+    echo "There are more python code than haskell"
+else
 
  
