3bf358515d5bbf0637eff5ea59d477dcc2d5a061
Harkanwar Singh Waraich <waraich@mills.cas.mcmaster.ca>
2018-02-25 00:37:25 -0500

    Updated files

--- ProjectAnalyze.sh
+++ ProjectAnalyze.sh
@@ -4,20 +4,21 @@ git status -uall
 }
 check_uncommited(){
 git diff $1 >> changes.log
-}
+}   
 create_todo(){
-(grep -r -h "#TODO" --exclude="ProjectAnanalyze.sh"--exclude="changes.log" --exclude="todo.log" .)  >> todo.log
+(grep -r -h "#TODO" --exclude="ProjectAnalyze.sh" --exclude="changes.log" --exclude="todo.log" .) > todo.log
+echo "todo.log created."
 }
 
 error_hs(){
-find -name "*.hs" -exec ghc -fno-code {} \;&>error.log
+find -name "*.hs" -exec ghc -fno-code {} \;>>error.log 2>&1
 }
 check_flies(){
 if [ -f ~/harry.hs ]
 then
-         echo "The file exists"
+     echo "The file exists"
 else
-         echo "the f does not exist"
+     echo "the f does not exist"
 
 fi
 }
