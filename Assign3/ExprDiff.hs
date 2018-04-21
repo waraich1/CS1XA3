88615abda452772fe9526a3c9644bd9dfdac60c9
waraich1 <35269086+waraich1@users.noreply.github.com>
2018-04-20 23:48:19 -0400

    Update ExprDiff.hs

--- Assign3/ExprDiff.hs
+++ Assign3/ExprDiff.hs
@@ -1,32 +1,36 @@
-{-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module ExprDiff where
 
-import           ExprType
-
-import qualified Data.Map.Strict as Map
+import ExprType
+import qualified Data.Map as Map
+import ExprPretty
 
 class DiffExpr a where
   eval :: Map.Map String a -> Expr a -> a
   simplify :: Map.Map String a -> Expr a -> Expr a
   partDiff :: String -> Expr a -> Expr a
 
+  {- Default Methods -}
   (!+) :: Expr a -> Expr a -> Expr a
-  e1 !+ e2 = simplify (Map.fromList []) $ Add e1 e2
+  e1 !+ e2 = simplify (Map.fromList [] ) $ Add e1 e2
+
   (!*) :: Expr a -> Expr a -> Expr a
-  e1 !* e2 = simplify (Map.fromList []) $ Mult e1 e2
+  e1 !* e2 = simplify (Map.fromList [] ) $ Mult e1 e2
+
   val :: a -> Expr a
   val x = Const x
+
   var :: String -> Expr a
   var x = Var x
 
-
 instance (Num a) => DiffExpr a where
-  eval vrs (Add e1 e2)  = eval vrs e1 + eval vrs e2
+  eval vrs (Add e1 e2) = eval vrs e1 + eval vrs e2
   eval vrs (Mult e1 e2) = eval vrs e1 * eval vrs e2
   eval vrs (Const x) = x
   eval vrs (Var x) = case Map.lookup x vrs of
-                       Just v  -> v
-                       Nothing -> error "failed lookup in eval"
-  simplify _ e = e -- #TODO finish me!
-  partDiff _ e = e -- #TODO finish me!
+                       Just v -> v
+                       Nothing -> error "failed lookup in eval!" -- In simplify, if there's nothing you just return Var x cause there's no value for it
+  simplify _ e = e -- #TODO finish me
+  partDiff _ e = e -- #TODO finish me
