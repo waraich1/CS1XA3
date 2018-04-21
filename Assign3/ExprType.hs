0e5f3f0cc41a291d0dddf2a7f6090ca7ae237ce3
waraich1 <35269086+waraich1@users.noreply.github.com>
2018-04-20 23:45:53 -0400

    Update ExprType.hs

--- Assign3/ExprType.hs
+++ Assign3/ExprType.hs
@@ -1,26 +1,26 @@
 module ExprType where
 
-import           Data.List
+import Data.List
 
 data Expr a = Add (Expr a) (Expr a)
             | Mult (Expr a) (Expr a)
+            | Cos (Expr a) -- ^ This constructor represents the cosine of an expression of type 'Expr a'
+            | Sin (Expr a) -- ^ This constructor represents the sin of an expression of type 'Expr a'
+            | Exp (Expr a) (Expr a) -- ^ This constructor represents the exponent of two expressions of type 'Expr a'. First expression to the power of second expression.
+            | NatExp (Expr a) -- ^ This constructor represents the Natural exponent of an expression of type 'Expr a'
+            | Ln (Expr a) -- ^  This constructor represents the Natural Logarithm of an expression of type 'Expr a'
             | Const a
             | Var String
-            | Sin ( Expr a)
-            | Cos (Expr a)
-            | Ln (Expr a) 
-            | Exp (Expr a)
-            | Power (Expr a) (Expr a)
-
   deriving Eq
 
+
 getVars :: Expr a -> [String]
-getVars (Add e1 e2) 	= getVars e1 `union` getVars e2
-getVars (Mult e1 e2)  	= getVars e1 `union` getVars e2
-getVars (Const _)     	= []
-getVars (Var ident)   	= [ident]
-getVars (Sin e1)	  	= getVars e1
-getVars (Cos e1)		= getVars e1
-getVars (Ln e1)			= getVars e1
-getVars (Exp e1)	  	= getVars e1
-getVars (Power e1 e1)  	= getVars e1 `union` getVarse e2
+getVars (Add e1 e2)  = getVars e1 `union` getVars e2
+getVars (Mult e1 e2) = getVars e1 `union` getVars e2
+getVars (Cos e1) = getVars e1
+getVars (Sin e1) = getVars e1
+getVars (Ln e1) = getVars e1
+getVars (NatExp e1) = getVars e1
+getVars (Exp e1 e2) = getVars e1 ++ getVars e2
+getVars (Const _)    = []
+getVars (Var ident)  = [ident]
